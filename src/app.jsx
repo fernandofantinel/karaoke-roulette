d22206aca425207295cd8cbb5a67871a36d0676f
Fernando Fantinel <fantinel89@gmail.com>
2024-09-25 15:48:10 -0300

    add `framer-motion` to animate the button

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Sponsor } from "./sponsor";
 import { Wheel } from "react-custom-roulette";
 import { useHotkeys } from "react-hotkeys-hook";
+import { motion } from "framer-motion";
 
 const rouletteOptions = [
   {
@@ -38,13 +39,12 @@ const rouletteOptions = [
 ];
 
 const easterEggSponsorMaxProbability = 19;
+const spinButton = document.getElementById("spinButton");
 
 export function App() {
   const [rouletteMustSpin, setRouletteMustSpin] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(0);
   const [easterEggSponsor, setEasterEggSponsor] = useState(false);
-
-  const spinButton = document.getElementById("spinButton");
 
   function spinRoulette() {
     if (rouletteMustSpin) {
@@ -77,27 +77,26 @@ export function App() {
           <h1 className="text-8xl">Karaoke Roulette</h1>
         </div>
         <div className="flex flex-col gap-8 mt-12">
-          <div>
-            <Wheel
-              mustStartSpinning={rouletteMustSpin}
-              prizeNumber={prizeNumber}
-              data={rouletteOptions}
-              backgroundColors={["#fcd34d", "#f59e0b"]}
-              textColors={["#451a03"]}
-              fontFamily="Inter"
-              spinDuration={0.6}
-              onStopSpinning={() => {
-                setRouletteMustSpin(false);
-              }}
-            />
-          </div>
-          <button
+          <Wheel
+            mustStartSpinning={rouletteMustSpin}
+            prizeNumber={prizeNumber}
+            data={rouletteOptions}
+            backgroundColors={["#fcd34d", "#f59e0b"]}
+            textColors={["#451a03"]}
+            fontFamily="Inter"
+            spinDuration={0.6}
+            onStopSpinning={() => {
+              setRouletteMustSpin(false);
+            }}
+          />
+          <motion.button
             onClick={() => spinRoulette()}
             id="spinButton"
             className="bg-amber-300 text-amber-950 text-xl font-bold py-3 rounded-xl hover:bg-amber-400"
+            whileTap={{ scale: 0.9 }}
           >
             Sortear
-          </button>
+          </motion.button>
         </div>
       </div>
 
